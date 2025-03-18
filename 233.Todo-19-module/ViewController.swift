7b3a4b32f78a74da8a7e983d946385f0c39ba160
latronixo <pamerra@mail.ru>
2025-03-18 13:12:16 +0300

    Initial Commit

---
+++ 233.Todo-19-module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  233.Todo-19-module
+//
+//  Created by Валентин Картошкин on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
